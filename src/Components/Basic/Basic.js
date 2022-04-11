75e73aab3e418abc28aae143de7f51c6dad13598
Fabian Villasenor <exionite@gmail.com>
2022-04-11 12:58:33 -0700

    features

--- src/Components/Basic/Basic.js
+++ src/Components/Basic/Basic.js
@@ -4,12 +4,29 @@ import "./Basic.scss"
 export default function Basic(props) {
     return (
         <>
-        {props.options.map((option)=>(
+            {props.options.map((option) => (
 
-            <div className="basic-card">
-                    {option.name}
+                <div className="basic-card" key={option.id}>
+                    <div className="card-title">
+                        <h3>{option.name}</h3>
+                    </div>
+                    <div className="card-price">
+                        {option.monthlyPrice}
+                    </div>
+                    <div className="card-info">
+                        <div className="card-features">
+                            <hr className='hr' />
+                            <div className="feature">{option.features[0]}</div>
+                            <hr className='hr' />
+                            <div className="feature">{option.features[1]}</div>
+                            <hr className='hr' />
+                            <div className="feature">{option.features[2]}</div>
+                            <hr className='hr' />
+                        </div>
+                    </div>
+                    <button>button</button>
                 </div>
-                ))}
+            ))}
         </>
     )
 }
