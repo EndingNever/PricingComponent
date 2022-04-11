85d81289e6d07575909114e53b5bf49973ca019b
Fabian Villasenor <exionite@gmail.com>
2022-04-11 15:23:56 -0700

    slider

--- src/Components/OurPricing/Pricing.js
+++ src/Components/OurPricing/Pricing.js
@@ -2,12 +2,19 @@ import React from 'react'
 import "./Pricing.scss"
 
 export default function Pricing() {
+    const onSlideEnabled=()=>{
+        
+    }
+    
     return (
         <div className='pricing-component'>
             <header className="header">
                 <h2>Our Pricing</h2>
                 <div className="toggle">
                     <p className='annually'>Annually</p>
+                    <div className="slider">
+                        <div onClick={onSlideEnabled} className="slider-button left-action"></div>
+                    </div>
                     <p className='monthly'>Monthly</p>
                 </div>
             </header>
