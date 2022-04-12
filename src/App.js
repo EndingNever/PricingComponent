76dbb7c6d71d40d032c06e7cd28d0ab73f0e1edf
Fabian Villasenor <exionite@gmail.com>
2022-04-12 14:36:19 -0700

    slider

--- src/App.js
+++ src/App.js
@@ -6,39 +6,44 @@ import Pricing from './Components/OurPricing/Pricing';
 function App() {
   const options = [
     {
-      id:0,
+      id: 0,
       name: "Basic",
       monthlyPrice: "19.99",
       yearlyPrice: "199.99",
       features: ["500 GB Storage", "2 Users Allowed", "Send up to 3 GB"],
     },
     {
-      id:1,
+      id: 1,
       name: "Professional",
       monthlyPrice: "24.99",
       yearlyPrice: "249.99",
       features: ["1TB Storage", "5 Users Allowed", "Send up to 10 GB"],
     },
     {
-      id:2,
+      id: 2,
       name: "Master",
       monthlyPrice: "39.99",
       yearlyPrice: "399.99",
       features: ["2 TB Storage", "10 Users Allowed", "Send up to 20 GB"],
     }
   ];
-  
+
   const [annually, setAnnually] = useState(false)
   const [slider, setSlider] = useState('right-action')
 
-  
+
   return (
     <div className="App">
       <div className="pricing-component-container">
-        <Pricing props={[annually, slider] }/>
+        <Pricing
+          annually={annually}
+          setAnnually={setAnnually}
+          slider={slider} 
+          setSlider={setSlider}
+          />
       </div>
       <div className="card-components-container">
-        <Basic options={options} slider={slider} annually={annually}/>
+        <Basic options={options} slider={slider} annually={annually} />
       </div>
     </div>
   );
--- src/Components/OurPricing/Pricing.js
+++ src/Components/OurPricing/Pricing.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react'
 import "./Pricing.scss"
 
-export default function Pricing() {
-    const [annually, setAnnually] = useState(false)
-    const [slider, setSlider] = useState('right-action')
-    const onSlideEnabled=()=>{
-        if(slider==='left-action'){
-            setSlider('right-action');
-            setAnnually(true);
-            console.log('going right', "annually is " + annually);
-        } else if (slider==='right-action'){
-            setSlider('left-action')
-            setAnnually(false)
-            console.log('going left',"annually is " +  annually);
+export default function Pricing(props) {
+    // const [annually, setAnnually] = useState(false)
+
+    // const [slider, setSlider] = useState('right-action')
+console.log(props.annually);
+    const onSlideEnabled = () => {
+        if (props.slider === 'left-action') {
+            props.setSlider('right-action');
+            props.setAnnually(true);
+            console.log('going right', "annually is " + props.annually);
+        } else if (props.slider === 'right-action') {
+            props.setSlider('left-action')
+            props.setAnnually(false)
+            console.log('going left', "annually is " + props.annually);
         }
     }
-    
+
     return (
         <div className='pricing-component'>
             <header className="header">
@@ -23,7 +25,7 @@ export default function Pricing() {
                 <div className="toggle">
                     <p className='annually'>Annually</p>
                     <div onClick={onSlideEnabled} className="slider">
-                        <div className={`slider-button ${slider}`}></div>
+                        <div className={`slider-button ${props.slider}`}></div>
                     </div>
                     <p className='monthly'>Monthly</p>
                 </div>
