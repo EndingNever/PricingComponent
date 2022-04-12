0d7145ca65eb03145e94bbac1d724e4f341bcfc9
Fabian Villasenor <exionite@gmail.com>
2022-04-12 14:44:25 -0700

    slider

--- src/Components/OurPricing/Pricing.js
+++ src/Components/OurPricing/Pricing.js
@@ -9,12 +9,10 @@ console.log(props.annually);
     const onSlideEnabled = () => {
         if (props.slider === 'left-action') {
             props.setSlider('right-action');
-            props.setAnnually(true);
-            console.log('going right', "annually is " + props.annually);
+            props.setAnnually(false);
         } else if (props.slider === 'right-action') {
             props.setSlider('left-action')
-            props.setAnnually(false)
-            console.log('going left', "annually is " + props.annually);
+            props.setAnnually(true)
         }
     }
 
