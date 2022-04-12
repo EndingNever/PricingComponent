bbf2411968ce3373d0c74a4a90f248e43e5dfb7f
Fabian Villasenor <exionite@gmail.com>
2022-04-12 15:39:05 -0700

    slider cursor

--- src/App.js
+++ src/App.js
@@ -2,32 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import Basic from './Components/Basic/Basic';
 import Pricing from './Components/OurPricing/Pricing';
+import { options } from './Components/Options/Options';
 
 function App() {
-  const options = [
-    {
-      id: 0,
-      name: "Basic",
-      monthlyPrice: "19.99",
-      yearlyPrice: "199.99",
-      features: ["500 GB Storage", "2 Users Allowed", "Send up to 3 GB"],
-    },
-    {
-      id: 1,
-      name: "Professional",
-      monthlyPrice: "24.99",
-      yearlyPrice: "249.99",
-      features: ["1TB Storage", "5 Users Allowed", "Send up to 10 GB"],
-    },
-    {
-      id: 2,
-      name: "Master",
-      monthlyPrice: "39.99",
-      yearlyPrice: "399.99",
-      features: ["2 TB Storage", "10 Users Allowed", "Send up to 20 GB"],
-    }
-  ];
-
   const [annually, setAnnually] = useState(false)
   const [slider, setSlider] = useState('right-action')
 
--- src/Components/OurPricing/Pricing.js
+++ src/Components/OurPricing/Pricing.js
@@ -5,12 +5,11 @@ export default function Pricing(props) {
     // const [annually, setAnnually] = useState(false)
 
     // const [slider, setSlider] = useState('right-action')
-console.log(props.annually);
     const onSlideEnabled = () => {
-        if (props.slider === 'left-action') {
+        if (props.slider === 'left-action') { // if slider is on left (annually) we set the slider to the right on click
             props.setSlider('right-action');
             props.setAnnually(false);
-        } else if (props.slider === 'right-action') {
+        } else if (props.slider === 'right-action') {  // if slider is right (monthly) we set it to annually on click
             props.setSlider('left-action')
             props.setAnnually(true)
         }
